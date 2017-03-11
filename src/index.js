2c3ee11b87df214afd324c6f951f7a40dbc4fe95
Kris Heinrich <krisheinrich@gmail.com>
2017-03-11 10:57:13 -0800

    add AirBnB-configured React linting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = "React Animations";
+const title = 'React Animations';
 
 ReactDOM.render(
   <div><h1>{title}</h1></div>,
-  document.getElementById('react-root')
+  document.getElementById('react-root'),  /*global document: true */
 );
 
 module.hot.accept();
