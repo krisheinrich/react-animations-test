29269bcb35471c12300075fd5f5f2a1b9e9399a2
Kris Heinrich <krisheinrich@gmail.com>
2017-03-11 09:51:02 -0800

    add react, render root component to DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-console.log("React Animations Test initializing...");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = "React Animations";
+
+ReactDOM.render(
+  <div><h1>{title}</h1></div>,
+  document.getElementById('react-root')
+);
 
 module.hot.accept();
